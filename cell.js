1e48da75436593c42d7c4e41e54556e257b9492c
kale1d <deneus@gmail.com>
2019-10-06 01:41:44 -0300

    WIP-Functionality for minesweeper ready. Flagged prop to do

--- cell.js
+++ cell.js
@@ -1,13 +1,12 @@
 //Properties of the Cell
-function Cell(x,y,w) {
-  this.x = x;
-  this.y = y;
+function Cell(i,j,w) {
+  this.i = i;
+  this.j = j;
+  this.x = i * w;
+  this.y = j * w;
   this.w = w;
-  if(random(1) < 0.5) {
-    this.mine = true;
-  } else {
-    this.mine = false;
-  }
+  this.adjacentCount = 0;
+  this.mine = false;
   this.revealed = false;
 };
 
@@ -22,6 +21,11 @@ Cell.prototype.show = function() {
     } else { 
       fill(200);
       rect(this.x, this.y, this.w, this.w);
+      if (this.adjacentCount > 0) {
+        textAlign(CENTER);
+        fill(0);
+        text(this.adjacentCount, this.x + this.w * 0.5 , this.y + this.w - 18); //Number of adjacent bombs and place it in the center
+      }
     }
   }
 }
@@ -32,6 +36,51 @@ Cell.prototype.contains = function (x, y) {
   return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
 }
 
+// Reveals the cell content
 Cell.prototype.reveal = function () {
   this.revealed = true;
+  if (this.adjacentCount === 0) {
+    this.cleanEmpty();
+  }
+}
+
+//Clean all empty adjacent cells (no mines around).
+Cell.prototype.cleanEmpty = function () {
+  for (let xOffset = -1; xOffset <=1; xOffset++) {
+    for (let yOffset = -1; yOffset <=1; yOffset++) {
+      let i = this.i + xOffset;
+      let j = this.j + yOffset;
+      //Checks if the cells has or not an adjacent cell.
+      if (i > -1 && i < cols && j > -1 && j < rows){
+        let adjacent = grid[i][j];
+        if (!adjacent.mine && !adjacent.revealed) {
+          adjacent.reveal()
+        }
+      }
+    }
+  }
+}
+//Checks if the cell has or not mines around it
+Cell.prototype.countMines = function () {
+  if (this.mine) {
+    this.adjacentCount = -1;
+    return;
+  }
+  let total = 0;
+
+  //Checks the surrounding cells 
+  for (let xOffset = -1; xOffset <=1; xOffset++) {
+    for (let yOffset = -1; yOffset <=1; yOffset++) {
+      let i = this.i + xOffset;
+      let j = this.j + yOffset;
+      //Checks if the cells has or not an adjacent cell.
+      if (i > -1 && i < cols && j > -1 && j < rows){
+        let adjacent = grid[i][j];
+        if (adjacent.mine) {
+          total++;
+        }
+      }
+    }    
+  }
+  this.adjacentCount = total;
 }
--- index.js
+++ index.js
@@ -7,22 +7,60 @@ for (let i = 0; i < arr.length; i++) {
 	return arr;
 }
 
-//Grid containing cells.
+//Global variables.
 let grid;
 let cols; 
 let rows;
 const w = 40;
 const h = 40;
+const totalMines = 10;
 
 //Setting up the board.
 function setup() {
 	createCanvas(401,401);
+
 	cols = floor(width / w); //Calculates the number of columns.
 	rows = floor(height / w); //Calculates the number of rows.
 	grid = createMatrix(cols,rows);
+
+// Create a cell for each column and row.
 	for (let i = 0; i < cols; i++) {
 		for (let j = 0; j < rows; j++) {
-			grid[i][j]=new Cell(i * w, j * w, w); // Create a cell for each column and row.
+			grid[i][j]=new Cell(i, j, w); 
+		}		
+	}
+// Creates an array with all the possible positions in the grid.
+let totalPositions = [];
+for (let i = 0; i < cols; i++) {
+	for (let j = 0; j < rows; j++) {
+		totalPositions.push([i, j]);			
+	}		
+}
+	//Random assign mines into a cell 
+	for (let k = 0; k < totalMines; k++) {
+		let index = floor(random(totalPositions.length));
+		let choice = totalPositions[index];
+		let i = choice[0];
+		let j = choice[1];
+		totalPositions.splice(index, 1); //Deletes that coordinate so it's no longer an option;
+		grid[i][j].mine = true;
+	}
+
+	// Returns the number of adjacent cells.
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			grid[i][j].countMines(); 
+		}		
+	}
+
+}
+
+//Ends the game if you click a mine.
+function gameOver() {
+	// Returns the number of adjacent cells.
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			grid[i][j].revealed = true; 
 		}		
 	}
 }
@@ -33,6 +71,10 @@ function mousePressed() {
 		for (let j = 0; j < rows; j++) {
 			if (grid[i][j].contains(mouseX, mouseY)) {
 				grid[i][j].reveal();
+
+				if (grid[i][j].mine) {
+					gameOver();
+				}
 			}
 		}		
 	}
